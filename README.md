40a7752409244120768ef15195fa8d3424648fff
Tal Hayut <talha@accessibe.com>
2022-09-10 23:19:02 +0300

    chore: init

--- README.md
+++ README.md
@@ -1,24 +1,14 @@
-# shellfie-cli
-create beautiful terminal screenshots using formatted string
+# upscaler-cli
+upscale images from your terminal
+
+# install
+```bash
+npm install upscaler-cli
+```
 
 # usage
 ```bash
-shellfie '\x1b[105mSHELLFIE\\x1b[0m🤳' '\x1b[38;5;225mthe easiest way' '\x1b[38;5;213mto create beautiful' '\x1b[38;5;14mCLI screenshots 📸' '\x1b[38;5;199mprogrammatically 🚀' -h 300 -w 400
+upscaler alien_landscape.png
 ```
-![](https://github.com/tool3/shellfie/blob/master/shellfies/shellfie.png?raw=true)   
-or
-```bash
-echo "\x1b[34mHello world" | shellfie -w 200 -h 200 -n hello
-```
-![](https://github.com/tool3/shellfie-cli/blob/master/shellfies/hello.png?raw=true)   
-
-# useful examples
-```bash
-lolcat --help > help.txt
-cat help.txt | shellfie -m raw
-```
-![](https://github.com/tool3/shellfied/blob/master/lolcat.png?raw=true)
-
-# options
-
-see [shellfie](https://github.com/tool3/shellfie)
+![](/alien_landscape.png)
+![](/alien_landscape_upscaled.png)
--- index.js
+++ index.js
@@ -1,36 +1,20 @@
 #!/usr/bin/env node
-require('./commands/load').load();
+
 const yargs = require('yargs');
-const shellify = require('./commands/shellify');
+const upscale = require('./commands/upscale');
 
 yargs
-    .config({
-        location: `${process.env.INIT_CWD || process.cwd()}/shellfies`
-    })
-    .command(
-        '$0 <string|string..>',
-        'create terminal screenshots from ansi string',
-        { },
-        async argv => {
-            try {
-                await shellify(argv);    
-            } catch (error) {
-                console.error(error);
-            }
-            
-        })
-    .example('$0 \x1b[32mI\'m Green! --width 200')
-    .option('name', { alias: 'n', type: 'string', desc: 'image name' })
-    .option('mode', { alias: 'm', type: 'string', desc: 'input mode (can also be \'raw\')', default: 'default'})
-    .option('width', { alias: 'w', type: 'number', desc: 'set width' })
-    .option('height', { alias: 'h', type: 'number', desc: 'set height' })
-    .option('background', { alias: 'b', type: 'string', desc: 'set background color' })
-    .option('foreground', { alias: 'f', type: 'string', desc: 'set default text color' })
-    .option('font-family', { alias: 'ff', type: 'string', desc: 'set font family' })
-    .option('font-size', { alias: 'fs', type: 'number', desc: 'set font size' })
-    .option('font-weight', { alias: 'fw', type: 'string', desc: 'set font weight' })
-    .option('location', { alias: 'l', type: 'string', desc: 'destination folder', default: './shellfies' })
-    .option('puppeteerArgs', { alias: 'p', type: 'array', desc: 'optional puppeteer args', default: ['--no-sandbox', '--disable-setuid-sandbox']})
-    .help()
-    .wrap(90)
-    .argv
+  .config({
+    location: `${process.env.INIT_CWD || process.cwd()}/shellfies`
+  })
+  .command('$0 <path>', 'upscale png images by 4x', {}, async (argv) => {
+    try {
+      await upscale(argv);
+    } catch (error) {
+      console.error(error);
+    }
+  })
+  .example('$0 image.png')
+  .option('name', { alias: 'n', type: 'string', desc: 'image name' })
+  .help()
+  .wrap(90).argv;
